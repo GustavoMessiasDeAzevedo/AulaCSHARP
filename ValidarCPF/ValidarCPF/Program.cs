9acb9491831d2d4d2365895cdf15f51830d71453
GustavoMessiasDeAzevedo <134647938+GustavoMessiasDeAzevedo@users.noreply.github.com>
2023-06-26 14:50:58 -0300

    Estrutura de repetição e máscara

--- ValidarCPF/ValidarCPF/Program.cs
+++ ValidarCPF/ValidarCPF/Program.cs
@@ -6,22 +6,19 @@ namespace ValidarCPF
     {
         static void Main(string[] args)
         {
-
             string confirmacao;
             do
             {
-
-                
                 Console.Write("Digite seu CPF (apenas números): ");
                 string num = Console.ReadLine();
 
                 if (!IsNumeric(num))
                 {
-
                     Console.WriteLine("Erro: O CPF deve conter apenas números.");
-                    break;
+                    Console.WriteLine("Deseja digitar novamente? (sim/não)");
+                    confirmacao = Console.ReadLine();
+                    continue;
                 }
-                
 
                 int[] numeroCPF = new int[11];
 
@@ -53,34 +50,36 @@ namespace ValidarCPF
                 char dig = Convert.ToChar(numeroCPF[9].ToString());
                 char dig2 = Convert.ToChar(numeroCPF[10].ToString());
 
+
+                string cpfNovo = ($"{num.Substring(0,3)}.{num.Substring(3, 3)}.{num.Substring(6, 3)}-{num.Substring(9, 2)}");
                 if (dig == digito1 && dig2 == digito2)
                 {
                     Console.WriteLine("CPF Aprovado");
+                    Console.WriteLine("============================");
+                    Console.WriteLine($"Seu CPF é: {cpfNovo}" );
+                    Console.WriteLine("============================");
                     break;
                 }
                 else
                 {
-
                     Console.WriteLine("CPF Negado");
-                    
                 }
-                Console.WriteLine("Deseja Tentar novamente?");
-                confirmacao = Console.ReadLine();
 
-            } while (confirmacao == "sim" || confirmacao == "Sim" || confirmacao == "S" || confirmacao == "s");
-            
+                Console.WriteLine("Deseja tentar novamente? (sim/não)");
+                confirmacao = Console.ReadLine();
+            } while (confirmacao.Equals("sim", StringComparison.OrdinalIgnoreCase) || confirmacao.Equals("s", StringComparison.OrdinalIgnoreCase));
         }
 
         static bool IsNumeric(string value)
         {
-           
-                foreach (char c in value)
+            foreach (char c in value)
+            {
+                if (!char.IsDigit(c))
                 {
-                    if (!char.IsDigit(c))
-                    {
-                        return false;
-                    }
+                    
+                    return false;
                 }
+            }
             return true;
         }
     }
