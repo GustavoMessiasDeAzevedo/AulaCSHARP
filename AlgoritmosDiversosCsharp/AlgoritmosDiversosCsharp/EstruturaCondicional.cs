3c7b3f9d272f26ada6d38fb13a406136a4b80057
GustavoMessiasDeAzevedo <134647938+GustavoMessiasDeAzevedo@users.noreply.github.com>
2023-06-27 16:31:57 -0300

    Estruturas de repetição e estrutura condicional

---
+++ AlgoritmosDiversosCsharp/AlgoritmosDiversosCsharp/EstruturaCondicional.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmosDiversosCsharp
+{
+    internal class EstruturaCondicional
+    {
+
+
+        static void Main()
+        {
+
+            Console.WriteLine("======== Estrutura Condicional ========");
+
+
+            int nota = 8;
+
+
+            //estrutura simples
+            if (nota >= 7)
+            {
+                Console.WriteLine("Aluno aprovado");
+            }
+
+            Console.WriteLine("=========================================================");
+
+            //estrutura composta
+            if (nota >= 7){
+                Console.WriteLine("Aluno aprovado");
+            }else{
+                Console.WriteLine("Aluno reprovado");
+            }
+
+            Console.WriteLine("=========================================================");
+
+            if(nota >= 9) {
+
+                Console.WriteLine("Aluno nota A");
+            }else if (nota >= 7) {
+
+                Console.WriteLine("Aluno nota B");
+            }else if (nota >= 5) {
+
+                Console.WriteLine("Aluno nota C");
+            }else if (nota >= 3){
+
+                Console.WriteLine("Aluno nota D");
+            }else{
+
+                Console.WriteLine("Aluno nota E");
+            }
+
+            Console.WriteLine("=========================================================");
+
+            string cor = "Vermelho";
+
+            switch (cor.ToLower()){
+
+                case "verde":
+                    Console.WriteLine("A cor é verde");
+                    break;
+
+                case "Vermelho":
+                    Console.WriteLine("A cor é vermelho");
+                    break;
+
+                case "Azul":
+                    Console.WriteLine("A cor é azul");
+                    break;
+
+                default:
+                    Console.WriteLine("A cor não foi reconhecida");
+                    break;
+
+            }
+
+        }
+            
+    
+    }
+}
---
+++ AlgoritmosDiversosCsharp/AlgoritmosDiversosCsharp/LacoDeRepeticao.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmosDiversosCsharp
+{
+    internal class LacoDeRepeticao
+    {
+
+        static void Main()
+        {
+
+            Console.WriteLine("======== Laço de repetição ========");
+
+            int contador = 0;
+
+            while(contador <= 5){
+
+                Console.WriteLine("O valor do contador é: " + contador);
+                contador++;
+            }
+
+            Console.WriteLine("=========================================================");
+
+            
+
+            for(contador = 0; contador <= 5; contador++){
+
+                Console.WriteLine("O valor do contador é: " + contador);
+            }
+
+            Console.WriteLine("=========================================================");
+
+            for (int i = 0; i <= 5; i++) {
+                Console.WriteLine("O valor do contador é: " + i);
+            }
+
+
+            Console.WriteLine("=========================================================");
+            contador = 0;
+            do
+            {
+                Console.WriteLine("O valor do contador é: " + contador);
+                contador ++;
+            }while(contador <= 5);
+
+
+            Console.WriteLine("=========================================================");
+
+
+            string[] frutas = { "maça", "banana", "laranja", "tomate" };
+
+            for(int i = 0; i < frutas.Length; i++) {
+                Console.WriteLine(frutas[i]);
+            }
+
+
+            Console.WriteLine("=========================================================");
+
+
+            foreach(string fruta in frutas) {  
+                Console.WriteLine(fruta);
+            }
+        }
+    }
+}
