e9ea7e3223b71bbf79772e5030a740a9e1a19192
GustavoMessiasDeAzevedo <134647938+GustavoMessiasDeAzevedo@users.noreply.github.com>
2023-06-28 14:43:02 -0300

    Código final do CPNJ com máscara e as situações

--- ValidarCNPJ/ValidarCNPJ/Program.cs
+++ ValidarCNPJ/ValidarCNPJ/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-
-
-
+using System.Text.RegularExpressions;
 
 namespace validar_cnpj
 {
@@ -11,79 +9,102 @@ namespace validar_cnpj
         {
             int[] multiplicacao = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
             int[] multiplicacao2 = { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
-           
-            Console.WriteLine("ESCERVA ");
-            string numero = Console.ReadLine();
-
-
-
-            int[] numerocnpj = new int[14];
-            for (int i = 0; i < 14; i++)
+            string confirmacao;
+            do
             {
-                numerocnpj[i] = int.Parse(numero[i].ToString());
-            }
+                Console.WriteLine("Escreva seu CNPJ: ");
+                string numero = Console.ReadLine();
 
+                string cpnjSemMascara = RemoverCaracteresEspeciais(numero);
+               
 
+                if (ContemLetras(numero))
+                {
+                    Console.WriteLine("Erro: O CNPJ não pode conter letras.");
+                    Console.WriteLine("Deseja digitar novamente? (sim/não)");
+                    confirmacao = Console.ReadLine();
+                    continue;
+                }
 
-            int soma = 0;
+                int[] numerocnpj = new int[14];
+                for (int i = 0; i < 14; i++)
+                {
+                    numerocnpj[i] = int.Parse(cpnjSemMascara[i].ToString());
+                }
 
-            for (int i = 0; i < 12; i++)
+                int soma = 0;
+                for (int i = 0; i < 12; i++)
+                {
+                    soma += numerocnpj[i] * multiplicacao[i];
+                }
+                int divisao = soma % 11;
+
+                if (divisao < 2)
+                {
+                    numerocnpj[12] = 0;
+                }
+                else
+                {
+                    numerocnpj[12] = 11 - divisao;
+                }
+
+                int soma2 = 0;
+                for (int i = 0; i < 13; i++)
+                {
+                    soma2 += numerocnpj[i] * multiplicacao2[i];
+                }
+                int divisao2 = soma2 % 11;
+
+                if (divisao2 < 2)
+                {
+                    numerocnpj[13] = 0;
+                }
+                else
+                {
+                    numerocnpj[13] = 11 - divisao;
+                }
+
+                char digito1 = numero[12];
+                char digito2 = numero[13];
+                char dig1 = Convert.ToChar(numerocnpj[12].ToString());
+                char dig2 = Convert.ToChar(numerocnpj[13].ToString());
+
+                if (digito1 == dig1 && digito2 == dig2)
+                {
+                    Console.WriteLine("Seu CNPJ está correto");
+                    Console.WriteLine("==================================");
+                    Console.WriteLine($"{numero}");
+                    Console.WriteLine("==================================");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Seu CNPJ está incorreto");
+                }
+
+                Console.WriteLine("Deseja tentar novamente? (sim/não)");
+                confirmacao = Console.ReadLine();
+            } while (confirmacao.Equals("sim", StringComparison.OrdinalIgnoreCase) || confirmacao.Equals("s", StringComparison.OrdinalIgnoreCase));
+        }
+
+        static string RemoverCaracteresEspeciais(string input)
+        {
+            string resultado = Regex.Replace(input, @"[^0-9]", "");
+            return resultado;
+        }
+
+        
+
+        static bool ContemLetras(string value)
+        {
+            foreach (char c in value)
             {
-                soma += numerocnpj[i] * multiplicacao[i];
+                if (char.IsLetter(c))
+                {
+                    return true;
+                }
             }
-            int divisao = soma % 11;
-
-            if (divisao < 2)
-            {
-                numerocnpj[12] = 0;
-            }
-            else
-            {
-                numerocnpj[12] = 11 - divisao;
-            }
-
-
-            int soma2 = 0;
-            for (int i = 0; i < 13; i++)
-            {
-
-                soma2 += numerocnpj[i] * multiplicacao2[i];
-
-            }
-              int divisao2 = soma2 % 11;
-
-            if (divisao2 < 2)
-            {
-                numerocnpj[13] = 0;
-            }
-            else
-            {
-                numerocnpj[13] = 11 - divisao;
-            }
-
-            char digito1 = numero[12];
-            char digito2 = numero[13];
-            char dig1 = Convert.ToChar(numerocnpj[12].ToString());
-            char dig2 = Convert.ToChar(numerocnpj[13].ToString());
-            string cnpjnovo = $"{numero.Substring(0, 2)}.{numero.Substring(2, 3)}.{numero.Substring(5, 3)}/{numero.Substring(8, 4)}-{numero.Substring(12, 2)}";
-
-
-
-            if (digito1 == dig1 && digito2 == dig2)
-            {
-                Console.WriteLine("Seu CNPJ ta certo");
-                Console.WriteLine("==================================");
-                Console.WriteLine($"{cnpjnovo}");
-                Console.WriteLine("==================================");
-            }
-            else
-            {
-                Console.WriteLine("Seu CNPJ está errado");
-            }
-
-
-
-
+            return false;
         }
     }
 }
