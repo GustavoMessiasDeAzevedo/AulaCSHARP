110fa1bc598d5baa088d0724eaf10b18a9a88bd0
GustavoMessiasDeAzevedo <134647938+GustavoMessiasDeAzevedo@users.noreply.github.com>
2023-06-28 16:48:57 -0300

    Versão final do forms CPF e CNPJ

---
+++ FrmSoftwareCPFeCNPJ/FrmSoftwareCPFeCNPJ/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FrmSoftwareCPFeCNPJ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.txtCPForCNPJ = new System.Windows.Forms.TextBox();
+            this.RadCPF = new System.Windows.Forms.RadioButton();
+            this.RadCNPJ = new System.Windows.Forms.RadioButton();
+            this.BtnValidar = new System.Windows.Forms.Button();
+            this.LblEntrada = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSair.ForeColor = System.Drawing.Color.Blue;
+            this.BtnSair.Location = new System.Drawing.Point(328, 299);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(168, 41);
+            this.BtnSair.TabIndex = 1;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = false;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // txtCPForCNPJ
+            // 
+            this.txtCPForCNPJ.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCPForCNPJ.Location = new System.Drawing.Point(173, 161);
+            this.txtCPForCNPJ.Name = "txtCPForCNPJ";
+            this.txtCPForCNPJ.Size = new System.Drawing.Size(362, 47);
+            this.txtCPForCNPJ.TabIndex = 2;
+            // 
+            // RadCPF
+            // 
+            this.RadCPF.AutoSize = true;
+            this.RadCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadCPF.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.RadCPF.Location = new System.Drawing.Point(109, 59);
+            this.RadCPF.Name = "RadCPF";
+            this.RadCPF.Size = new System.Drawing.Size(72, 29);
+            this.RadCPF.TabIndex = 3;
+            this.RadCPF.TabStop = true;
+            this.RadCPF.Text = "CPF";
+            this.RadCPF.UseVisualStyleBackColor = true;
+            // 
+            // RadCNPJ
+            // 
+            this.RadCNPJ.AutoSize = true;
+            this.RadCNPJ.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadCNPJ.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.RadCNPJ.Location = new System.Drawing.Point(355, 59);
+            this.RadCNPJ.Name = "RadCNPJ";
+            this.RadCNPJ.Size = new System.Drawing.Size(85, 29);
+            this.RadCNPJ.TabIndex = 4;
+            this.RadCNPJ.TabStop = true;
+            this.RadCNPJ.Text = "CNPJ";
+            this.RadCNPJ.UseVisualStyleBackColor = true;
+            // 
+            // BtnValidar
+            // 
+            this.BtnValidar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnValidar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnValidar.ForeColor = System.Drawing.Color.Blue;
+            this.BtnValidar.Location = new System.Drawing.Point(63, 299);
+            this.BtnValidar.Name = "BtnValidar";
+            this.BtnValidar.Size = new System.Drawing.Size(168, 41);
+            this.BtnValidar.TabIndex = 5;
+            this.BtnValidar.Text = "Validar";
+            this.BtnValidar.UseVisualStyleBackColor = false;
+            this.BtnValidar.Click += new System.EventHandler(this.BtnValidar_Click);
+            // 
+            // LblEntrada
+            // 
+            this.LblEntrada.AutoSize = true;
+            this.LblEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblEntrada.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblEntrada.Location = new System.Drawing.Point(29, 169);
+            this.LblEntrada.Name = "LblEntrada";
+            this.LblEntrada.Size = new System.Drawing.Size(138, 39);
+            this.LblEntrada.TabIndex = 6;
+            this.LblEntrada.Text = "Entrada";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.MediumOrchid;
+            this.ClientSize = new System.Drawing.Size(562, 411);
+            this.Controls.Add(this.LblEntrada);
+            this.Controls.Add(this.BtnValidar);
+            this.Controls.Add(this.RadCNPJ);
+            this.Controls.Add(this.RadCPF);
+            this.Controls.Add(this.txtCPForCNPJ);
+            this.Controls.Add(this.BtnSair);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Validador de CPF e CNPJ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnSair;
+        private System.Windows.Forms.TextBox txtCPForCNPJ;
+        private System.Windows.Forms.RadioButton RadCPF;
+        private System.Windows.Forms.RadioButton RadCNPJ;
+        private System.Windows.Forms.Button BtnValidar;
+        private System.Windows.Forms.Label LblEntrada;
+    }
+}
+
---
+++ FrmSoftwareCPFeCNPJ/FrmSoftwareCPFeCNPJ/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmSoftwareCPFeCNPJ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnValidar_Click(object sender, EventArgs e)
+        {
+            if (!RadCNPJ.Checked && !RadCPF.Checked){
+
+                MessageBox.Show("Selecione o CPF ou o CNPJ", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+
+            }
+      
+
+            if (RadCPF.Checked){
+
+                string numeroString = txtCPForCNPJ.Text;
+                int[] numeroCPF = new int[11];
+
+                if (Letras(numeroString))
+                {
+
+                    MessageBox.Show("O CPF aceita apenas numeros", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (numeroString.Length >= 15)
+                {
+                    MessageBox.Show("O número de caracteres foi ultrapassado", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                string cpfSemMascara = RemoverCaracteresEspeciais(numeroString);
+
+
+                for(int i = 0; i < 11; i++) {
+                    numeroCPF[i] = int.Parse(cpfSemMascara[i].ToString());
+    
+                }
+
+                int soma = 0;
+
+                for (int i = 0; i < 9; i++){
+                    soma += numeroCPF[i] * (10 - i);
+                }
+
+                int div = (soma * 10) % 11;
+                if(div == 10){
+                    numeroCPF[9] = 0;
+                }else{
+                    numeroCPF[9] = div;
+                }
+
+
+                int soma2 = 0;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    soma2 += numeroCPF[i] * (11 - i);
+                }
+
+                int div2 = (soma2 * 10) % 11;
+                if (div2 == 11)
+                {
+                    numeroCPF[10] = 0;
+                }
+                else
+                {
+                    numeroCPF[10] = div2;
+                }
+
+                char digito1 = cpfSemMascara[9];
+                char digito2 = cpfSemMascara[10];
+                char dig = Convert.ToChar(numeroCPF[9].ToString());
+                char dig2 = Convert.ToChar(numeroCPF[10].ToString());
+
+
+                if (dig == digito1 && dig2 == digito2)
+                {
+
+                    MessageBox.Show("Seu CPF é válido", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }else {
+
+                    MessageBox.Show("Seu CPF é inválido", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            if(RadCNPJ.Checked) {  
+            
+                string numeroStringCpnj = txtCPForCNPJ.Text;
+
+                if (numeroStringCpnj.Length >= 19)
+                {
+                    MessageBox.Show("O número de caracteres foi ultrapassado", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                string cnpjSemMascara = RemoverCaracteresEspeciais(numeroStringCpnj);
+
+                if (Letras(numeroStringCpnj)){
+
+                    MessageBox.Show("O CNPJ aceita apenas numeros", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+
+                int[] numeroCNPJ = new int[14];
+                int[] multiplicacao = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+                int[] multiplicacao2 = { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+                for (int i = 0; i < 14; i++) {
+
+                    numeroCNPJ[i] = int.Parse(cnpjSemMascara[i].ToString());
+                }
+
+                int soma = 0;
+
+                for (int i = 0; i < 12; i++){
+
+                    soma += numeroCNPJ[i] * multiplicacao[i];
+                }
+
+                int divisao = soma % 11;
+
+                numeroCNPJ[12] = (divisao < 2) ? 0 : 11 - divisao;
+
+                int soma2 = 0;
+                for (int i = 0; i < 13; i++)
+                {
+
+                    soma2 += numeroCNPJ[i] * multiplicacao2[i];
+                }
+
+                int divisao2 = soma2 % 11;
+
+                numeroCNPJ[13] = (divisao2 < 2) ? 0 : 11 - divisao2;
+
+                char digito1 = cnpjSemMascara[12];
+                char digito2 = cnpjSemMascara[13];
+                char dig1 = Convert.ToChar(numeroCNPJ[12].ToString());
+                char dig2 = Convert.ToChar(numeroCNPJ[13].ToString());
+
+                
+  
+
+
+                if (dig1 == digito1 && dig2 == digito2)
+                {
+
+                    MessageBox.Show("Seu CNPJ é válido", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }else{
+
+                    MessageBox.Show("Seu CNPJ é inválido", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+            static string RemoverCaracteresEspeciais(string input){
+
+            string resultado = Regex.Replace(input, @"[^0-9]", "");
+            return resultado;
+
+            }
+
+            static bool Letras (string value) {  
+            
+            foreach (char c in value) {
+
+                if (char.IsLetter(c)){
+                    return true;
+                }
+            }
+                return false;
+            }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
