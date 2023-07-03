cc57e808658f4aed8ba3a053f8d126c77f0ddefd
GustavoMessiasDeAzevedo <134647938+GustavoMessiasDeAzevedo@users.noreply.github.com>
2023-07-03 17:12:01 -0300

    Login baseado no projeto

---
+++ FrmLoginProjeto/FrmLoginProjeto/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace FrmLoginProjeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblLogin = new System.Windows.Forms.Label();
+            this.LblSenha = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnLogar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblLogin
+            // 
+            this.LblLogin.AutoSize = true;
+            this.LblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblLogin.Location = new System.Drawing.Point(241, 59);
+            this.LblLogin.Name = "LblLogin";
+            this.LblLogin.Size = new System.Drawing.Size(86, 31);
+            this.LblLogin.TabIndex = 0;
+            this.LblLogin.Text = "Email";
+            // 
+            // LblSenha
+            // 
+            this.LblSenha.AutoSize = true;
+            this.LblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSenha.Location = new System.Drawing.Point(241, 220);
+            this.LblSenha.Name = "LblSenha";
+            this.LblSenha.Size = new System.Drawing.Size(97, 31);
+            this.LblSenha.TabIndex = 1;
+            this.LblSenha.Text = "Senha";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(142, 254);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(295, 47);
+            this.txtSenha.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(142, 93);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(295, 47);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // btnLogar
+            // 
+            this.btnLogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogar.Location = new System.Drawing.Point(97, 336);
+            this.btnLogar.Name = "btnLogar";
+            this.btnLogar.Size = new System.Drawing.Size(175, 47);
+            this.btnLogar.TabIndex = 5;
+            this.btnLogar.Text = "Login";
+            this.btnLogar.UseVisualStyleBackColor = true;
+            this.btnLogar.Click += new System.EventHandler(this.btnLogar_Click_1);
+            // 
+            // btnListar
+            // 
+            this.btnListar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListar.Location = new System.Drawing.Point(320, 336);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(175, 47);
+            this.btnListar.TabIndex = 6;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 472);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnLogar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.LblSenha);
+            this.Controls.Add(this.LblLogin);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblLogin;
+        private System.Windows.Forms.Label LblSenha;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnLogar;
+        private System.Windows.Forms.Button btnListar;
+    }
+}
+
---
+++ FrmLoginProjeto/FrmLoginProjeto/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmLoginProjeto
+{
+    public partial class Form1 : Form
+    {
+        private List<string> listaEmail = new List<string>(); // Mover a declaração das listas para o escopo da classe
+        private List<string> listaSenha = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+            string senha = txtSenha.Text;
+
+            if (email.Length == 0 && senha.Length == 0)
+            {
+                MessageBox.Show("Nenhum campo foi preenchido.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            
+            listaEmail.Add(txtEmail.Text);
+            listaSenha.Add(txtSenha.Text);
+
+            txtEmail.Clear();
+            txtSenha.Clear();
+
+            MessageBox.Show("Seu email e senha foram listados com sucesso.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnLogar_Click_1(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+            string senha = txtSenha.Text;
+
+            if (listaEmail.Contains(email) && listaSenha.Contains(senha)) // Corrigir a condição de verificação
+            {
+                MessageBox.Show("Login bem-sucedido!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Email ou senha inválidos.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            txtEmail.Clear();
+            txtSenha.Clear();
+        }
+    }
+}
+
